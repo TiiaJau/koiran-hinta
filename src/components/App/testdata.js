ba6778c0ddaacbd87a19e1c532e396b199f8ac81
TiiaJau <tiia.jauhiainen@edu.sasky.fi>
2023-11-19 13:53:05 +0200

    lisää sovelluksen testidatan

---
+++ src/components/App/testdata.js
@@ -0,0 +1,59 @@
+const testdata = [
+    {
+      id:          "1",
+      type:        "Ruoka",
+      amount:      89.90,
+      paymentDate: "2023-08-01",
+      periodStart: "2023-08-01",
+      periodEnd:   "2023-10-31",
+      receiver:    "Peten koiratarvike Oy"
+    },
+    {
+      id:          "2",
+      type:        "Lelut",
+      amount:      25.00,
+      paymentDate: "2023-05-06",
+      periodStart: "",
+      periodEnd:   "",
+      receiver:    "Kivuton Oy"
+    },
+    {
+      id:          "3",
+      type:        "Eläinlääkäri",
+      amount:      207.30,
+      paymentDate: "2023-10-10",
+      periodStart: "",
+      periodEnd:   "",
+      receiver:    "Joensuun eläinklinikka"
+    },  
+    {
+      id:          "4",
+      type:        "Vakuutus",
+      amount:      205.00,
+      paymentDate: "2023-01-01",
+      periodStart: "2023-01-01",
+      periodEnd:   "2023-03-31",
+      receiver:    "IF Vahinkovakuutusyhtiö Oy"
+    },
+    {
+      id:          "5",
+      type:        "Koulutus",
+      amount:      100.00,
+      paymentDate: "2023-09-09",
+      periodStart: "",
+      periodEnd:   "",
+      receiver:    "Polvijärven Koiraharrastajat Ry"
+    },
+    {
+      id:          "6",
+      type:        "Vakuutus",
+      amount:      205.00,
+      paymentDate: "2023-04-01",
+      periodStart: "2023-04-01",
+      periodEnd:   "2023-06-30",
+      receiver:    "IF Vahinkovakuutusyhtiö Oy"
+    } 
+  ]
+  
+  export default testdata
+  
