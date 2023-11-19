d78cca8d3eb847ba6f75398594d2efa62f6a1707
TiiaJau <tiia.jauhiainen@edu.sasky.fi>
2023-11-19 09:38:38 +0200

    lisää Header-komponentin

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import styles from './Header.module.scss'
+
+function Header() {
+
+  return (
+    <div className={styles.header}>
+      Koiran hinta
+    </div>
+  )
+
+}
+
+export default Header
