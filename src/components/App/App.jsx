4b57766dcc64cb2a1571facc143e803702d578b8
TiiaJau <tiia.jauhiainen@edu.sasky.fi>
2023-11-19 15:08:43 +0200

    lisää lomaketietojen tallennuksen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react'
 import testdata from './testdata.js'
 import AppRouter from '../AppRouter'
 
 function App() {
 
+  const [data, setData] = useState(testdata)
+
+  const handleItemSubmit = (newitem) => {
+    let copy = data.slice()
+    copy.push(newitem)
+    copy.sort( (a,b) => {
+      const aDate = new Date(a.paymentDate)
+      const bDate = new Date(b.paymentDate)
+      return bDate - aDate
+    })
+    setData(copy)
+  }
+
   return (
     <>
-      <AppRouter data={testdata} />
+      <AppRouter data={data} onItemSubmit={handleItemSubmit} />  
     </>
   )
 }
