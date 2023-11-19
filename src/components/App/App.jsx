72f156795d164765dfbbf31a771161f68d2dec58
TiiaJau <tiia.jauhiainen@edu.sasky.fi>
2023-11-19 13:33:30 +0200

    lisää AppRouter-komponentin ja reitityksen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,13 @@
-import Settings from '../Settings'
-import Stats from '../Stats'
-import Items from '../Items'
-import { ButtonContainer } from '../../shared/buttons'
-import { FloatingButton } from '../../shared/buttons'
-import Content from '../Content'
-import Menu from '../Menu'
-import Header from '../Header'
-import styles from './App.module.scss'
+import AppRouter from '../AppRouter'
 
 function App() {
 
   return (
     <>
-      <ButtonContainer>
-      <div className={styles.app}>
-        <Header />
-        <Content>
-        <Settings />
-        </Content>
-        <Menu />
-      </div>
-      </ButtonContainer>
+      <AppRouter />
     </>
   )
 }
 
 export default App
+
