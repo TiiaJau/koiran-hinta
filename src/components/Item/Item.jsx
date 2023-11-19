f912fc6dcb9d2201a20a093cf22df08d45ac8521
TiiaJau <tiia.jauhiainen@edu.sasky.fi>
2023-11-19 09:12:06 +0200

    lisää Item-komponentin

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,21 @@
+import styles from './Item.module.scss'
+
+function Item() {
+
+    return (
+        <div className={styles.item}>
+          <div className={styles.item_data}>
+            <div className={styles.item_type}>Ruoka</div>
+            <div className={styles.item_amount}>89,90 €</div>
+            <div className={styles.item_date}>20.3.2023</div>
+            <div className={styles.item_timespan}>1.8.2023 - 31.10.2023</div>
+            <div className={styles.item_receiver}>Peten koiratarvike Oy</div>
+            <div className={styles.item_average}>29,97 €/kk</div>
+          </div>
+        </div>
+      )
+    
+  }
+  
+  export default Item
+  
