448130ae53d00d44218923b1f6a6b7dbee150c95
TiiaJau <tiia.jauhiainen@edu.sasky.fi>
2023-11-19 13:23:43 +0200

    lisää Settings-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import Settings from '../Settings'
 import Stats from '../Stats'
 import Items from '../Items'
 import { ButtonContainer } from '../../shared/buttons'
@@ -15,7 +16,7 @@ function App() {
       <div className={styles.app}>
         <Header />
         <Content>
-        <Stats />
+        <Settings />
         </Content>
         <Menu />
       </div>
